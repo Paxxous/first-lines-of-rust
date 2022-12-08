1d6347b5099091e816bb1ad6d49341e2d2652590
Benjamin <itsenveehio@gmail.com>
2022-12-08 19:38:37 +0000

    comments and error handling.

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,27 @@ use rand::Rng;
 fn guessing() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("Guess a number between 1 and 100.");
-    // println!("{secret_number}");
 
+    // Loop it.
     loop {
+        // Take in input via string
         let mut input = String::new();
         io::stdin()
             .read_line(&mut input)
             .expect("Please input a valid string.");
 
-        let converted_input: u32 = input
-            .trim()
-            .parse()
-            .expect("Please input a number.");
+        // Convert and handle errors if anything happens
+        let input: u32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                continue;
+            },
+        };
 
-        match converted_input.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+        // Compare the input to the secret number
+        match input.cmp(&secret_number) {
+            Ordering::Less => println!("{} is too small!", input),
+            Ordering::Greater => println!("{} is too big!", input),
             Ordering::Equal => {
                 println!("Correct!");
                 break;
