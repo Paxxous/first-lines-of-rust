a75e2d0740625a5f002829a16042f125d5098209
Benjamin <itsenveehio@gmail.com>
2022-12-09 21:47:34 +0000

    discovery

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 fn main() {
-    let y = {
-        let x = 3;
-        x + 1 // Won't return a value??? Semicolons means that it's a statement??
-        // WAIT HOLY SHIT THAT'S HOW YOU RETURN VALUE IN FUNCTION
-    };
+    let x = literally_just_returns_five();
+    println!("{x}"); // WOAHHHHHH
+}
 
-    println!("The value of y is {y}");
+fn literally_just_returns_five() -> i32 {
+    5
 }
 
 // https://doc.rust-lang.org/book/ch03-03-how-functions-work.html#statements-and-expressions
