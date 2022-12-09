4727f7552dda09202715ef1598cca298cccd5e82
Benjamin <itsenveehio@gmail.com>
2022-12-09 21:20:02 +0000

    Functino arguments, I'm literally just relearning the basics here...

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    functon();
+    another_function(20);
 }
 
-fn functon() {
-    println!("Woah woah woah woah woah wwaoh functino");
+fn another_function(x: i32) {
+    println!("Function really said {x}");
 }
 
 // https://doc.rust-lang.org/book/ch03-03-how-functions-work.html
