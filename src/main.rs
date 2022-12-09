28753b3e5aa9075a0abdea5f3de23ce4e0e753c8
Benjamin <itsenveehio@gmail.com>
2022-12-09 21:50:37 +0000

    comments?

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,16 @@ fn main() {
     println!("{x}"); // WOAHHHHHH
 }
 
+/*
+ * Bro I already know how functions work.
+ * Look at me with these fancy comments
+ * I've been only using these for adding insight
+ * to what I am doing.
+ */
+
 fn literally_just_returns_five() -> i32 {
-    5
+    let x = 99;
+    x + 1
 }
 
 // https://doc.rust-lang.org/book/ch03-03-how-functions-work.html#statements-and-expressions
