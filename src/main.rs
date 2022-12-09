178f32719f908bc60768a0fde3e0830b9ae51bbb
Benjamin <itsenveehio@gmail.com>
2022-12-09 18:38:32 +0000

    I know about functions but i'm still going to read the documentation

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,9 @@
-use std::io;
-
 fn main() {
-    let a = [1, 2, 3, 6, 7, 88];
-
-    let mut index = String::new();
-    io::stdin()
-        .read_line(&mut index)
-        .expect("PLease input a valid string");
-
-    // use usize when indexing some form of collection, (such as an array)
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Please input a number");
-
-    let element = a[index];
-
-    println!("{}", element);
+    functon();
 }
 
-// https://doc.rust-lang.org/book/ch03-02-data-types.html
+fn functon() {
+    println!("Woah woah woah woah woah wwaoh functino");
+}
+
+// https://doc.rust-lang.org/book/ch03-03-how-functions-work.html
