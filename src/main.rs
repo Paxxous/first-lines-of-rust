b03532b8462a8ceb07c087829bf73dfa42b06def
Benjamin <itsenveehio@gmail.com>
2022-12-09 21:24:56 +0000

    I'm still learning it though...

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    another_function(20);
+    another_function(20, 'a');
 }
 
-fn another_function(x: i32) {
-    println!("Function really said {x}");
+fn another_function(x: i32, y: char) {
+    println!("Function really said {x} and {y}");
 }
 
-// https://doc.rust-lang.org/book/ch03-03-how-functions-work.html
+// https://doc.rust-lang.org/book/ch03-03-how-functions-work.html#statements-and-expressions
