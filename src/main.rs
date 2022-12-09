ccab0d5ff871ab9030b297e6258c6b3cb3f07e5a
Benjamin <itsenveehio@gmail.com>
2022-12-09 16:26:25 +0000

    another tuple lmao

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 // the main function 
 fn main() {
-    // Tuples woah!
-    // We can declare this explicitly :D
-    let tupe = (10, 5.25, 68.01, 0xff);
-    let (_a, _b, _c, d) = tupe;
-
-    println!("{d}");
+    const HEXATUPLES: (i32, u32) = (0xa, 0xff);
+    println!("{}", HEXATUPLES.1);
 }
 
 // https://doc.rust-lang.org/book/ch03-02-data-types.html
