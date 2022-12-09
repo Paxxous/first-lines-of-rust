3695101419db6536e1742eaac3c1af8c4e8130a8
Benjamin <itsenveehio@gmail.com>
2022-12-09 21:44:54 +0000

    finding about returns in rust, (this is why you have to relearn everything.)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
-    another_function(20, 'a');
-}
+    let y = {
+        let x = 3;
+        x + 1 // Won't return a value??? Semicolons means that it's a statement??
+        // WAIT HOLY SHIT THAT'S HOW YOU RETURN VALUE IN FUNCTION
+    };
 
-fn another_function(x: i32, y: char) {
-    println!("Function really said {x} and {y}");
+    println!("The value of y is {y}");
 }
 
 // https://doc.rust-lang.org/book/ch03-03-how-functions-work.html#statements-and-expressions
