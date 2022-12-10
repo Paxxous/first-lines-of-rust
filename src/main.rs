f9deb4f014f81be94606edcc4ec52cf7965debd6
Benjamin <itsenveehio@gmail.com>
2022-12-10 23:47:21 +0000

    Control flow. I know this.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
 fn main() {
-    let x = literally_just_returns_five();
-    println!("{x}"); // WOAHHHHHH
+    let num = 5;
+
+    if num < 3 {
+        println!("Condition true");
+    } else {
+        println!("was false");
+    }
 }
 
-/*
- * Bro I already know how functions work.
- * Look at me with these fancy comments
- * I've been only using these for adding insight
- * to what I am doing.
- */
-
-fn literally_just_returns_five() -> i32 {
-    let x = 99;
-    x + 1
-}
-
-// https://doc.rust-lang.org/book/ch03-03-how-functions-work.html#statements-and-expressions
+// https://doc.rust-lang.org/book/ch03-05-control-flow.html
