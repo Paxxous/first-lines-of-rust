26adcef7a5cf1c079616b3ed9d8b6b2520479ce6
Benjamin <itsenveehio@gmail.com>
2022-12-12 15:40:49 +0000

    simple if else

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
-fn main() {
-    let num = 5;
+use std::io;
 
-    if num < 3 {
-        println!("Condition true");
+fn main() {
+    let number = number_input();
+
+    if number == 15 {
+        println!("Good choice ;)");
     } else {
-        println!("was false");
+        println!("YOUR NUMBER IS NOT FUCKING 15");
     }
+}
+
+fn number_input() -> u32 {
+    let mut number = String::new();
+    io::stdin()
+        .read_line(&mut number)
+        .expect("Failed to read line");
+
+    let number: u32 = number
+        .trim()
+        .parse()
+        .expect("Please input a number");
+
+    number
 }
 
 // https://doc.rust-lang.org/book/ch03-05-control-flow.html
