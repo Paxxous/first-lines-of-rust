1c94bbe538058081fedf6a4eddd517de5e9bbde3
Benjamin <itsenveehio@gmail.com>
2022-12-13 14:07:24 -0500

    if else in a delclaration... sick!

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
 fn main() {
-    let x = 3;
+    let isit = true;
+    let x = if isit { 5 } else { 6 }; // This is new...
 
-    // Relly? Get me to the good stuff >:(
-    if x % 3 == 0 {
-        println!("The number is divisivble by three :0");
-    } else if x % 2 == 0 {
-        println!("Your number is dibisible by two.")
-    } else {
-        println!("This number is not divisible by three or two >:(.")
-    }
+    println!("{x}");
 }
 
 // https://doc.rust-lang.org/book/ch03-05-control-flow.html
