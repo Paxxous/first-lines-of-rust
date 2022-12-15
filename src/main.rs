f90254c4323bb825c77b472e9c43193ec06771c5
Benjamin <itsenveehio@gmail.com>
2022-12-15 08:56:58 -0500

    Woah a counter!

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,15 @@
-use std::io;
-
-const SPECIAL_NUMBER: u32 = 69;
-
+const COUNT_UNTIL: i64 = 4123489999;
 fn main() {
-    let result_from_it: bool = loop {
-        let mut input = String::new();
+    let mut counter = 0;
+    let end = loop {
+        counter += 1;
 
-        println!("Type in the special number to win.");
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Couldn't read line");
-
-        let input: u32 = match input.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please input a valid number...");
-                continue
-            },
-        };
-
-        if input == SPECIAL_NUMBER {
-            break true;
+        if counter == COUNT_UNTIL {
+            break counter;
         }
     };
 
-    if result_from_it == true {
-        println!("You did it! You got the correct number, {}", SPECIAL_NUMBER);
-    } else {
-        println!("Pussy.");
-    }
+    println!("ended with result of {}", end);
 }
 
 // https://doc.rust-lang.org/book/ch03-05-control-flow.html#repetition-with-loops
