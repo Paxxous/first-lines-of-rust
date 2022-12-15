81dfe94bf81dbe59adec27ee7ad5da9cf4143af4
Benjamin <itsenveehio@gmail.com>
2022-12-14 19:03:42 -0500

    Made a cool program with loops!

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
 use std::io;
 
+const SPECIAL_NUMBER: u32 = 69;
+
 fn main() {
-    let mut buffer = String::new();
-    io::stdin()
-        .read_line(&mut buffer)
-        .expect("Failed to read line");
-    
-    loop {
-        // Merry Easter!!!!
-        println!("Look I'm copying you! {}", buffer);
+    let result_from_it: bool = loop {
+        let mut input = String::new();
+
+        println!("Type in the special number to win.");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Couldn't read line");
+
+        let input: u32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input a valid number...");
+                continue
+            },
+        };
+
+        if input == SPECIAL_NUMBER {
+            break true;
+        }
+    };
+
+    if result_from_it == true {
+        println!("You did it! You got the correct number, {}", SPECIAL_NUMBER);
+    } else {
+        println!("Pussy.");
     }
 }
 
