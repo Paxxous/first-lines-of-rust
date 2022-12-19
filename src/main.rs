f9beaaf470f8485957ff8d1962efc27fab2998ca
Benjamin <itsenveehio@gmail.com>
2022-12-19 11:37:15 -0500

    reverse a loop :0

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,9 @@
-use std::io;
-
 fn main() {
-    loop {
-        // Grab input from the console
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
-
-        // Translate to int and handle errors
-        let input: f64 = match input.trim().parse() {
-            Ok(num) => {
-                println!("Please input a number thank you!");
-                num
-            },
-            Err(_) => continue,
-        };
-
-        println!("You're number plus 69.420 is {}", input + 69.420);
-
-        if input == 0.0 {
-            break;
-        }
+    for i in (1..=69).rev() {
+        println!("{}", i);
     }
 
-    println!("Thank you for playing");
+    println!("LIFTOFF");
 }
 
 // https://doc.rust-lang.org/book/ch03-05-control-flow.html#loop-labels-to-disambiguate-between-multiple-loops
