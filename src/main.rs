71fdef261e5e8254068130d344cbcb302e4b5cfb
Benjamin <itsenveehio@gmail.com>
2023-03-27 12:02:30 -0400

    oh noes

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,5 @@
-struct Client {
-    connected: bool,
-    name: String,
-    times_logged_in: u64,
-}
-
-fn build_client(
-        connected: bool,
-        name: String,
-        times_logged_in: u64
-    ) -> Client {
-
-    Client {
-        connected,
-        name,
-        times_logged_in,
-    }
-}
-
 fn main() {
-    let client = build_client(
-        true,
-        String::from("the"),
-        1
-    );
-
-    println!("client online: {} client name: {}, times logged in: {}",
-        client.connected,
-        client.name,
-        client.times_logged_in,
-    );
 }
 
-// https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html
+// oh boy
+// https://doc.rust-lang.org/book/ch05-02-example-structs.html
