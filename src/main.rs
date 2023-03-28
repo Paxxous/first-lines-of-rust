ebb344683c3ef9e7752982f5d1778f3ed21b6c6b
Benjamin <itsenveehio@gmail.com>
2023-03-28 14:57:57 -0400

    owah!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+#[derive(Debug)]
 struct Rectangle {
     w: u32,
     h: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        // godamina
+        self.w * self.h
+    }
 }
 
 fn main() {
@@ -9,7 +17,7 @@ fn main() {
         h: 20
     };
 
-    println!("Fucntion {:?}", re);
+    println!("Fucntion {:?} and {}", re, re.area());
 }
 
 // https://doc.rust-lang.org/book/ch05-02-example-structs.html
