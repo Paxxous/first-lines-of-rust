32a3537db4349fb53ca27b9710dd6671dec4aaaa
Benjamin <itsenveehio@gmail.com>
2023-03-28 14:50:33 -0400

    structs are cool :)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
-fn main() {
+struct Rectangle {
+    w: u32,
+    h: u32,
 }
 
-// oh boy
+fn main() {
+    let re = Rectangle {
+        w: 10,
+        h: 20
+    };
+
+    println!("Fucntion {:?}", re);
+}
+
 // https://doc.rust-lang.org/book/ch05-02-example-structs.html
