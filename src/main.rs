d7570c05b2a711cf3625ecee7e69586b7d886d72
Benjamin <itsenveehio@gmail.com>
2023-04-03 09:36:16 -0400

    finished up structs

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,26 @@
-#[derive(Debug)]
 struct Rectangle {
     w: u32,
     h: u32,
 }
 
 impl Rectangle {
-    fn width(&self) -> bool {
-        // huuh?? oh you can add logic :D
-        self.w > 0
-    }
-
     fn holds(&self, o: &Rectangle) -> bool {
         self.w > o.w && self.h > o.h
+    }
+
+    // initializer for new type or something??
+    fn square(size: u32) -> Self {
+        Self {
+            w: size,
+            h: size,
+        }
     }
 }
 
 fn main() {
-    let re = Rectangle {
-        w: 10,
-        h: 20
-    };
+    let sqr = Rectangle::square(100);
 
-    let is = Rectangle {
-        w: 230498,
-        h: 8902598,
-    };
-
-    if is.holds(&re) {
-        println!("It's too FAT.")
-    }
+    println!("w: {}, h: {}", sqr.w, sqr.h);
 }
 
 // https://doc.rust-lang.org/book/ch05-03-method-syntax.html#wheres-the---operator
