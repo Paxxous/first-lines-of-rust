15354639201cea8593fa3c8f3bc4d8cfed3eea6f
Benjamin <itsenveehio@gmail.com>
2023-04-21 18:53:54 -0400

    one billion! owah!

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,27 @@
-use std::io;
-
-#[derive(Debug)]
-struct Internet {
-    service: String,
-    ip: String,
-    ping: u32,
+struct YourSelf {
+    name: String,
+    level: u64,
 }
 
-// work?
-enum Terminal {}
-
-impl Terminal {
-    fn grab(prompt: String) -> String {
-        println!("{}", prompt);
-
-        let mut str = String::new();
-        io::stdin()
-            .read_line(&mut str)
-            .expect("Failed to read line");
-
-        str
+impl YourSelf {
+    fn improve(&mut self) {
+        self.level = self.level + 1;
     }
 }
 
 fn main() {
-    println!("Hello there my good friend ;)\nWe're nearly done setting up your internet");
-    
-    let user_service = Terminal::grab(
-        String::from("What service would you like?")
-    );
+    println!("Hello world");
 
-    println!("Thank you for the input ;)");
-
-    let this_fuckers_internet = Internet {
-        service: user_service,
-        ip: String::from("69.420.6.9"),
-        ping: 69420, // in ms
+    let mut you = YourSelf {
+        name: String::from("haba"),
+        level: 0,
     };
 
-    println!("Here's some data ab our internet mg: {:#?}\n(ping is in ms)", this_fuckers_internet);
+    for _ in 1..=1000000000 {
+        you.improve();
+    }
+
+    println!("Now {} is at level {}", you.name, you.level);
 }
 
 // https://doc.rust-lang.org/book/ch06-01-defining-an-enum.html
