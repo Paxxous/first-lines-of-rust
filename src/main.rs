75342a666d95f81fe004ba1785033e5d7b2e788a
Benjamin <itsenveehio@gmail.com>
2023-06-29 10:28:10 -0400

    big projec???

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,5 @@
-struct YourSelf {
-    name: String,
-    level: u64,
-}
-
-impl YourSelf {
-    fn improve(&mut self) {
-        self.level = self.level + 1;
-    }
-}
-
 fn main() {
-    println!("Hello world");
 
-    let mut you = YourSelf {
-        name: String::from("haba"),
-        level: 0,
-    };
-
-    for _ in 1..=1000000000 {
-        you.improve();
-    }
-
-    println!("Now {} is at level {}", you.name, you.level);
 }
 
-// https://doc.rust-lang.org/book/ch06-01-defining-an-enum.html
+// https://doc.rust-lang.org/book/ch06-00-enums.html
